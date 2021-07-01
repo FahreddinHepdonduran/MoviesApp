64748fc55471e2436b7187700e998baf8e59092e
FahreddinHepdonduran <fahreddinhepdonduran@gmail.com>
2021-07-01 11:23:31 +0300

    Initial Commit

---
+++ TopRatedMovies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TopRatedMovies
+//
+//  Created by fahreddin on 1.07.2021.
+//  Copyright © 2021 fahreddin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
