6fc0b0d0f229d57492d24f8958ab2781578d4542
FahreddinHepdonduran <fahreddinhepdonduran@gmail.com>
2021-07-01 12:16:39 +0300

    Add Movie Model

---
+++ TopRatedMovies/Models/Movie.swift
@@ -0,0 +1,37 @@
+//
+//  Movie.swift
+//  TopRatedMovies
+//
+//  Created by fahreddin on 1.07.2021.
+//  Copyright © 2021 fahreddin. All rights reserved.
+//
+
+import Foundation
+
+struct Movie {
+  let id: Int
+  let originalLanguage: String
+  let originalTitle: String
+  let overview: String
+  let popularity: Double
+  let imagePath: String
+  let releaseDate: String
+  let title: String
+  let voteAverage: Double
+  let voteCount: Int
+}
+
+extension Movie {
+  enum CodingKeys: String, CodingKey {
+    case id
+    case originalLanguage = "original_language"
+    case originalTitle = "original_litle"
+    case overview
+    case popularity
+    case imagePath = "poster_path"
+    case releaseDate = "release_date"
+    case title
+    case voteAverage = "vote_average"
+    case voteCount = "vote_count"
+  }
+}
