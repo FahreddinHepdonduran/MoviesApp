8d0f3bbf1cfcc95dd5776ac6f463a9275599ff36
FahreddinHepdonduran <fahreddinhepdonduran@gmail.com>
2021-07-01 12:19:35 +0300

    Add Movies model

---
+++ TopRatedMovies/Models/Movies.swift
@@ -0,0 +1,13 @@
+//
+//  Movies.swift
+//  TopRatedMovies
+//
+//  Created by fahreddin on 1.07.2021.
+//  Copyright © 2021 fahreddin. All rights reserved.
+//
+
+import Foundation
+
+struct Movies: Codable {
+  let movies: [Movie]
+}
